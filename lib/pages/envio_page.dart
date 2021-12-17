eaddd6be9aa35b9ec7ab7a5047bf5987fe9e7de0
Nicole Herbas <78878905+Nicole-Hf@users.noreply.github.com>
2021-12-17 18:33:42 -0400

    envio

---
+++ lib/pages/envio_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class EnvioPage extends StatefulWidget {
+  // ignore: prefer_const_declarations
+  static final routeName = 'envio';
+
+  @override
+  _EnvioPageState createState() => _EnvioPageState();
+}
+
+class _EnvioPageState() extends State<EnvioPage> {
+  String _pais = '';
+  String _ciudad = '';
+  String _direccion = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
