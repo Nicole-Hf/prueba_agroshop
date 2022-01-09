f6aefebafa7749bad67c01ad50eb92394da2f991
Nicole Herbas <78878905+Nicole-Hf@users.noreply.github.com>
2022-01-08 21:09:29 -0400

    Update pdf_page.dart

--- lib/pages/pdf_page.dart
+++ lib/pages/pdf_page.dart
@@ -1,4 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:prueba_agroshop/model/factura.dart';
+import 'package:prueba_agroshop/model/factura_item.dart';
+import 'package:prueba_agroshop/services/api.dart';
+import 'package:prueba_agroshop/services/pdf_api.dart';
+import 'package:prueba_agroshop/services/pdf_factura.dart';
+import 'package:prueba_agroshop/variables.dart';
 
 // ignore: use_key_in_widget_constructors
 class PdfPage extends StatefulWidget {
@@ -7,6 +15,28 @@ class PdfPage extends StatefulWidget {
 }
 
 class _PdfPageState extends State<PdfPage> {
+  var factura = <Factura>[];
+
+  @override
+  void initState() {
+    _getFactura();
+    super.initState();
+  }
+
+  _getFactura() async {
+    await _initData();
+  }
+
+  //llamada a las API
+  _initData() async {
+    await CallApi().getPublicData("factura/$idFactura").then((response) {
+      setState(() {
+        Iterable list = json.decode(response.body);
+        factura = list.map((model) => Factura.fromJson(model)).toList();
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
         backgroundColor: Colors.black,
@@ -22,10 +52,6 @@ class _PdfPageState extends State<PdfPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                /*TitleWidget(
-                  icon: Icons.picture_as_pdf,
-                  text: 'Generate Invoice',
-                ),*/
                 const SizedBox(height: 48),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40)),
@@ -33,78 +59,21 @@ class _PdfPageState extends State<PdfPage> {
                   child: FittedBox(child: Text('Invoice PDF', style: TextStyle(fontSize: 20, color: Colors.white),),),          
                   onPressed: () async {
                     final date = DateTime.now();               
-
-                    final invoice = Invoice(
-                      supplier: Supplier(
-                        name: 'Sarah Field',
-                        address: 'Sarah Street 9, Beijing, China',
-                        paymentInfo: 'https://paypal.me/sarahfieldzz',
-                      ),
-                      customer: Customer(
-                        name: 'Apple Inc.',
-                        address: 'Apple Street, Cupertino, CA 95014',
-                      ),
-                      info: InvoiceInfo(
-                        date: date,
-                        dueDate: dueDate,
-                        description: 'My description...',
-                        number: '${DateTime.now().year}-9999',
-                      ),
+                    final invoice = Factura(
+                      nroFactura: factura[1].nroFactura, 
+                      codControl: factura[1].codControl,
+                      fecha: date,
+                      nit: factura[1].nit,
+                      total: factura[1].total,
                       items: [
-                        InvoiceItem(
-                          description: 'Coffee',
-                          date: DateTime.now(),
-                          quantity: 3,
-                          vat: 0.19,
-                          unitPrice: 5.99,
+                        FacturaItem(
+                          nombre: 'Coffee',
+                          cantidad: 4,
+                          subtotal: 3,
                         ),
-                        InvoiceItem(
-                          description: 'Water',
-                          date: DateTime.now(),
-                          quantity: 8,
-                          vat: 0.19,
-                          unitPrice: 0.99,
-                        ),
-                        InvoiceItem(
-                          description: 'Orange',
-                          date: DateTime.now(),
-                          quantity: 3,
-                          vat: 0.19,
-                          unitPrice: 2.99,
-                        ),
-                        InvoiceItem(
-                          description: 'Apple',
-                          date: DateTime.now(),
-                          quantity: 8,
-                          vat: 0.19,
-                          unitPrice: 3.99,
-                        ),
-                        InvoiceItem(
-                          description: 'Mango',
-                          date: DateTime.now(),
-                          quantity: 1,
-                          vat: 0.19,
-                          unitPrice: 1.59,
-                        ),
-                        InvoiceItem(
-                          description: 'Blue Berries',
-                          date: DateTime.now(),
-                          quantity: 5,
-                          vat: 0.19,
-                          unitPrice: 0.99,
-                        ),
-                        InvoiceItem(
-                          description: 'Lemon',
-                          date: DateTime.now(),
-                          quantity: 4,
-                          vat: 0.19,
-                          unitPrice: 1.29,
-                        ),
-                      ],
+                      ], id: factura[1].id, pagoID: factura[1].pagoID,
                     );
-
                     final pdfFile = await PdfInvoiceApi.generate(invoice);
-
                     PdfApi.openFile(pdfFile);
                   },
                 ),
